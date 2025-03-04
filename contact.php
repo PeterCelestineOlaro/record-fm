5f7db902223b706258ebeda67c7e293db0b41594
PeterCelestineOlaro <olaropetercelestine@gmail.com>
2025-03-04 23:02:20 +0300

    changes tot rh contact page

--- contact.php
+++ contact.php
@@ -2,61 +2,7 @@
 require 'ola.php';
 ?>
 <!-- HEADER -->
-    <header class="bg-secondary-dark sticky-top">
-        <div class="r-container">
-            <nav class="navbar navbar-expand-lg">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="index.html">
-                        <div class="logo-container d-flex align-items-center">
-                            <img src="image/Record-App-Logo-512.png" alt="logo" class="img-fluid" style="height: 50px; margin-right: 10px;">
-                            <h4 class="mb-0" style="font-weight: 900;">97.7 Record FM</h4> 
-                        </div>
-                        
-                    </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <i class="fa-solid fa-bars-staggered"></i>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mx-auto mb-2 mb-lg-0 gap-3">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="about_us.html">About Us</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="about_us.html">News </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="podcasts.html">Gallery</a>
-                            </li>
-                           
-                           
-                            <li class="nav-item">
-                                <a class="nav-link" href="contact.html">Contact Us</a>
-                            </li>
-                        </ul>
-                        <div class="social-container mb-lg-0 mb-3">
-                            <a href="#" class="social-item">
-                                <i  class="fa-brands fa-youtube"></i>
-                            </a>
-                            <a href="#" class="social-item">
-                                <i class="fa-brands fa-tiktok"></i>
-                            </a>
-                            <a href="#" class="social-item">
-                                <i class="fa-brands fa-instagram"></i>
-                            </a>
-                            <a href="#" class="social-item">
-                                <i class="fa-brands fa-twitter"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </header>
+
     <!-- END HEADER -->
 
     <!-- MAIN CONTENT -->
